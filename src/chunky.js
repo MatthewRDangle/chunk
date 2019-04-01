d70eedb8a4fa9257807f34492e4dcab73bb9645f
Matthew R Dangle <MatthewRDangle@gmail.com>
2019-04-01 11:08:22 -0400

    Outlined the idea of Chunky.

---
+++ src/chunky.js
@@ -0,0 +1,17 @@
+/*
+ * The purpose of a chunk right now is to build out put an HTML component, automate use case documentation, and to create reusable rendering functions.
+*/
+
+let chunk =  {
+	name: 'button',
+	descr: '',
+	modifiers: '',
+	prefix: 'chunk',
+	tag: 'div',
+	attr: {},
+	render: function(variables) {
+		/*
+		Build logic. The developer should be able to use any framework they want here that may produce a component. 
+		 */
+	}
+}
---
+++ src/chunkyDoc.js
@@ -0,0 +1,4 @@
+/*
+** Automate documentation builder for Chunky.
+** [WIP] Should be able to loop through all chunks and build documentation of HTML output, CSS classes used, modifiers, description, ect.
+*/
