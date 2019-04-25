f0ddf4f459ee1383613e5fd75f44dceea8def792
Matthew R Dangle <MatthewRDangle@gmail.com>
2019-04-25 12:23:16 -0400

    Expanded constructor and wrapped it in a ify.

--- src/chunky.js
+++ src/chunky.js
@@ -2,25 +2,51 @@
 // METHODS FOR CHUNK
 //====================================================================
 
-/*
- * The Chunk Web Component Prototype Object.
+/* 
+ * Object to remember all methods for Chunky. 
+ * Wrapped function around methods to protect global space.
 */
-function Chunk(name, type) {
-	
-	/* Initialize Base Attributes */
-	if (!name || typeof name !== 'string')
-		throw Error('A chunk must be given a name.');
-	this.name = name;
+var chunky = {};
+(function() {
 
-	if (!type || typeof type !== 'string' )
-		this.type = 'component';
-	else
-		this.type = type;
+	/*
+	 * The Chunk Web Component Prototype Constructor.
+	*/
+	chunky.constructor = function (name, type, prefix) {
+		
+		/* Validate and Assign Name for Chunk. */
+		if (!name || typeof name !== 'string')
+			throw Error('A chunk must be given a name.');
+		this.name = name;
+
+		/* Validate and Assign Type for Chunk. */
+		if (!type || typeof type !== 'string' )
+			this.type = 'component'; // Default Value for Chunk.
+		else
+			this.type = type;
+		
+		/* Assign default prefix is none exists. */
+		if (!prefix)
+			this.prefix = 'chunk';
+		else
+			this.prefix = prefix;
+		
+		/* Create a container to hold attributes. */
+		this.attr = {};	
+		
+		/* Enable Build Controls */
+		this.build = null;
+	}
 	
 	/* Enable Rendering */
-	this.render = function() {
-		var chunk = document.createElement("div");
-		chunk.className = this.name;
-		return chunk;
+	chunky.constructor.prototype.render = function() {
+		
+		/* Check to see if build is set up correctly. */
+		if (typeof this.build !== 'function') {
+			throw Error('There are no build instructions for chunk');
+		}
+		
+		/* Return build. */
+		return this.build();
 	}
-}
+})
