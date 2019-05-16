da0ef29e6d16c0dcffabc50af68bd34b0382cbd8
Matthew R Dangle <MatthewRDangle@gmail.com>
2019-05-16 11:49:33 -0400

    Added error checking for createMod method.

--- src/chunky.js
+++ src/chunky.js
@@ -13,49 +13,64 @@
 */
 function chunk (name, type, prefix) {
 
-	/* Validate and Assign Name for Chunk. */
+	// Validate and Assign Name for Chunk.
 	if (!name || typeof name !== 'string')
 		throw Error('A chunk must be given a name.');
 	this.name = name;
 
-	/* Assign default ClassName prefix if none exists. */
+	// Assign default ClassName prefix if none exists.
 	if (!prefix)
 		this.prefix = 'chunk';
 	else
 		this.prefix = prefix;
 	
-	/* Validate and Assign Type for Chunk. */
+	// Validate and Assign Type for Chunk.
 	if (!type || typeof type !== 'string' )
 		this.type = 'component'; // Default Value for Chunk.
 	else
 		this.type = type;
 	
-	/* Create a modification tree. */
+	// Create a modification tree.
 	this.mods = {};
 	
-	/* Create place holder for build controls. */
+	// Create place holder for build controls.
 	this.build = null;
 }
 
 /*
  * Method: Add Modification.
  * Description: Add an attribute to the HTML.
+ * 
+ * @param name string - The object key reference for this modification.
+ * @param value unset - The value of the modification.
+ * @param container string - Sub object tree for organized storage.
  */
 chunk.prototype.createMod = function(name, value, container) {
+
+	// Check to see if modification
+	if (!name || tyeof name !== 'string')
+		throw Error('A new modification must have a name of type String.');
 	
-	/* Check to see if name will be placed inside container. */
+	// Check to see if name will be placed inside container.
 	if (container && typeof container === "string") {
 		
-		/* Create the container is the container does not already exist. */
+		// Create the container is the container does not already exist.
 		if (!this.mods[container])
 			this.mods[container] = {};
 		
-		/* Add mod to container. */
-		var container = this.mods[container];
-		container[name] = value;
+		// Check to see if container is not an existing mod.
+		if (typeof this.mods[container] !== 'object') {
+			throw Error('A mod storage container can not have the same name as a mod.');
+		}
+		else {
+			
+			// Add mod to container.
+			var container = this.mods[container];
+			container[name] = value;	
+		}
 	}
 
-	/* Add mod to the base if it does not exist. */
+	// Add mod to the base if it does not exist.
 	this.mods[name] = value;
 }
 	
@@ -65,11 +80,11 @@ chunk.prototype.createMod = function(name, value, container) {
 */
 chunk.prototype.print = function(string) {
 	
-	/* Get a String if it does not exist. */
+	// Get a String if it does not exist.
 	if (!string)
 		string = this.build(this).outerHTML;
 	
-	/* Return legible HTML. */
+	// Return legible HTML.
 	return String(string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 }
 
@@ -81,12 +96,12 @@ chunk.prototype.render = function(chunk) {
 	if (!chunk || !chunk.type)
 		chunk = this;
 		
-	/* Check to see if build is set up correctly. */
+	// Check to see if build is set up correctly.
 	if (typeof chunk.build !== 'function') {
 		throw Error('There are no build instructions for ' + chunk.prefix + ' ' + chunk.name);
-		return ''; /* Leave the function and don't attempt to build. */
+		return ''; // Leave the function and don't attempt to build.
 	}
 
-	/* Return build. */
+	// Return build.
 	return chunk.build(chunk);
 }
